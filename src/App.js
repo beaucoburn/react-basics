e221cb2b124bfed641df26dea74f1c29d03b6e7f
Beau Coburn <douglas.coburn@gmail.com>
2023-04-19 11:04:36 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import {
+  useRef,
+  useState
+} from "react";
 import './App.css';
 
 function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  const plus = (e) => {
+    e.preventDefault();
+    setResult((result) => result + Number
+    (inputRef.current.value));
+  };
+
+  const minus = (e) => {
+    e.preventDefault();
+    setResult((result) => result - Number
+    (inputRef.current.value));
+  };
+
+  const times = (e) => {
+    e.preventDefault();
+    setResult((result) => result * Number
+    (inputRef.current.value));
+  };
+
+  const divide = (e) => {
+    e.preventDefault();
+    setResult((result) => result / Number
+    (inputRef.current.value));
+  };
+
+  const resetInput = (e) => {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  };
+
+  const resetResult = (e) => {
+    e.preventDefault();
+    setResult((prevVal) => prevVal * 0);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+          />
+          <button onClick={plus}>Add</button>
+          <button onClick={minus}>Subtract</button>
+          <button onClick={times}>Multiply</button>
+          <button onClick={divide}>Divide</button>
+          <button onClick={resetInput}>Reset Input</button>
+          <button onClick={resetResult}>Reset Result</button>
+      </form>
     </div>
   );
 }
